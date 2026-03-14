3645f0d402230259cfc373f9911f7181004a87a0
sarim-alis <sarimslayerali786@gmail.com>
2026-03-14 20:33:30 +0500

    feats

--- scraper.py
+++ scraper.py
@@ -9,7 +9,7 @@ import time
 
 def scrape_priceoye_by_category():
 
-    url = "https://priceoye.pk/mobiles/infinix"
+    url = "https://priceoye.pk/mobiles/oppo"
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get(url)
