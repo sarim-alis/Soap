7e15867f8f7a44b1baee67d89ca447b938ec86d2
sarim-alis <sarimslayerali786@gmail.com>
2026-03-14 17:49:32 +0500

    feat: realme

--- scraper.py
+++ scraper.py
@@ -8,8 +8,9 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def scrape_priceoye_by_category():
 
+    url   = "https://priceoye.pk/mobiles/realme"
     # url = "https://priceoye.pk/mobiles/samsung"
-    url = "https://priceoye.pk/mobiles/apple"
+    # url = "https://priceoye.pk/mobiles/apple"
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get(url)
